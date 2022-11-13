aebdb951b09ab4451438b93ac76f01fa31eb1bbf
Edmund Miller <edmund.a.miller@protonmail.com>
2022-11-13 15:56:49 -0600

    style: Fix all eslint errors

--- src/functions.ts
+++ src/functions.ts
@@ -1,11 +1,16 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import retry = require('async-retry')
-import semver = require('semver')
+import * as tc from '@actions/tool-cache'
+import {GitHub} from '@actions/github/lib/utils'
+import retry from 'async-retry'
+import semver from 'semver'
 
 const NEXTFLOW_REPO = {owner: 'nextflow-io', repo: 'nextflow'}
 
-async function all_nf_releases(ok) {
+// HACK Private but I want to test this
+export async function all_nf_releases(
+  ok: InstanceType<typeof GitHub>
+): Promise<object> {
   return await ok.paginate(
     ok.rest.repos.listReleases,
     NEXTFLOW_REPO,
@@ -13,7 +18,10 @@ async function all_nf_releases(ok) {
   )
 }
 
-async function latest_stable_release_data(ok) {
+// HACK Private but I want to test this
+export async function latest_stable_release_data(
+  ok: InstanceType<typeof GitHub>
+): Promise<object> {
   const {data: stable_release} = await ok.rest.repos.getLatestRelease(
     NEXTFLOW_REPO
   )
@@ -21,9 +29,12 @@ async function latest_stable_release_data(ok) {
   return stable_release
 }
 
-export async function release_data(version, ok) {
+export async function release_data(
+  version: string,
+  ok: InstanceType<typeof GitHub>
+): Promise<object> {
   // Setup tag-based filtering
-  let filter = r => {
+  let filter = (r: object): boolean => {
     return semver.satisfies(r.tag_name, version, true)
   }
 
@@ -32,7 +43,8 @@ export async function release_data(version, ok) {
   if (version.includes('latest')) {
     if (version.includes('-everything')) {
       // No filtering
-      filter = r => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      filter = (r: object) => {
         return true
       }
     } else if (version.includes('-edge')) {
@@ -50,7 +62,7 @@ export async function release_data(version, ok) {
   // Get all the releases
   const all_releases = await all_nf_releases(ok)
 
-  let matching_releases = all_releases.filter(filter)
+  const matching_releases = all_releases.filter(filter)
 
   matching_releases.sort(function (x, y) {
     semver.compare(x.tag_name, y.tag_name, true)
@@ -59,13 +71,13 @@ export async function release_data(version, ok) {
   return matching_releases[0]
 }
 
-export function nextflow_bin_url(release, get_all) {
+export function nextflow_bin_url(release, get_all: boolean): string {
   const release_assets = release.assets
   const all_asset = release_assets.filter(a => {
     return a.browser_download_url.endsWith('-all')
   })[0]
   const regular_asset = release_assets.filter(a => {
-    return a.name == 'nextflow'
+    return a.name === 'nextflow'
   })[0]
 
   const dl_asset = get_all ? all_asset : regular_asset
@@ -73,9 +85,13 @@ export function nextflow_bin_url(release, get_all) {
   return dl_asset.browser_download_url
 }
 
-export async function install_nextflow(url, version) {
+export async function install_nextflow(
+  url: string,
+  version: string
+): Promise<string> {
   core.debug(`Downloading Nextflow from ${url}`)
   const nf_dl_path = await retry(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async bail => {
       return await tc.downloadTool(url)
     },
