b9a7fb94999a1afa4002ae4ed2c87fc60fc1d261
Edmund Miller <edmund.a.miller@protonmail.com>
2022-11-12 23:00:23 -0600

    refactor: Move functions functions to their own file

---
+++ test/functions.ts
@@ -0,0 +1,7 @@
+import test from 'ava'
+
+test.todo('all_nf_releases')
+test.todo('lastest_stable_release_data')
+test.todo('release_data')
+test.todo('nextflow_bin_url')
+test.todo('install_nextflow')
--- test/main.ts
+++ test/main.ts
@@ -1,8 +1,8 @@
-import test from 'ava';
+import test from 'ava'
 
-const fn = () => 'foo';
+const fn = () => 'foo'
 
 test('fn() returns foo', t => {
   t.pass()
   // t.is(fn(), 'foo');
-});
+})
