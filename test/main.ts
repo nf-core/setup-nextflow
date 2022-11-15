2138b34480f888f48b434fbaaf653ddd67f2cdf1
Edmund Miller <edmund.a.miller@protonmail.com>
2022-11-15 09:57:34 -0600

    style: Use double-quotes
    Co-authored-by: ewels <ewels@users.noreply.github.com>

--- test/main.ts
+++ test/main.ts
@@ -1,13 +1,13 @@
-import test from 'ava' // eslint-disable-line import/no-unresolved
-import * as cp from 'child_process'
-import * as path from 'path'
-import * as process from 'process'
+import test from "ava" // eslint-disable-line import/no-unresolved
+import * as cp from "child_process"
+import * as path from "path"
+import * as process from "process"
 
 // eslint-disable-next-line ava/no-skip-test
-test.skip('test runs', t => {
-  process.env['INPUT_VERSION'] = 'v22.10.2'
+test.skip("test runs", t => {
+  process.env["INPUT_VERSION"] = "v22.10.2"
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'src', 'main.js')
+  const ip = path.join(__dirname, "..", "lib", "src", "main.js")
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
--- test/utils.ts
+++ test/utils.ts
@@ -1,9 +1,9 @@
 export function getToken(first: boolean): string {
-  const token = process.env['GITHUB_TOKEN'] || ''
+  const token = process.env["GITHUB_TOKEN"] || ""
   if (!token && first) {
     /* eslint-disable-next-line no-console */
     console.warn(
-      'Skipping GitHub tests. Set $GITHUB_TOKEN to run REST client and GraphQL client tests'
+      "Skipping GitHub tests. Set $GITHUB_TOKEN to run REST client and GraphQL client tests"
     )
     first = false
   }
