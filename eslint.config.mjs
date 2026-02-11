1dbc5fe504f192b5798838088d3f9ad9a9608be4
mashehu <mashehu3@gmail.com>
2026-02-11 15:08:39 +0100

    add dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,9 @@ import tseslint from "typescript-eslint"
 import globals from "globals"
 
 export default tseslint.config(
+  {
+    ignores: ["dist/**", "lib/**"]
+  },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
